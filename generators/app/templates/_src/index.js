6f864b176c51df99c955a9926f9606c2a7bbc18e
Bravi <bravilogy@gmail.com>
2017-07-08 00:44:46 +0100

    fix paths

--- generators/app/templates/_src/index.js
+++ generators/app/templates/_src/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-require('./app.scss');
+require('./scss/app.scss');
 
 require('./index.html');
 
-const Elm = require('./Main.elm');
+const Elm = require('./elm/Main.elm');
 const app = Elm.Main.embed(document.getElementById('main'));
